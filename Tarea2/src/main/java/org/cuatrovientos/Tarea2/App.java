ebd8f1ceeb321f5249de12d70f5c05900352cc4d
Aitor <Aitor@192.168.1.40>
2015-09-23 16:51:06 +0200

    The program completed

--- Tarea2/src/main/java/org/cuatrovientos/Tarea2/App.java
+++ Tarea2/src/main/java/org/cuatrovientos/Tarea2/App.java
@@ -12,8 +12,8 @@ import javax.naming.spi.DirStateFactory.Result;
 
 
 /**
- * Hello world!
- *
+ * @Author Aitor Jauregi
+ * A simple program for administrate a data base.
  */
 public class App 
 {
@@ -38,74 +38,28 @@ public class App
     	
     	
     	do{
-    		System.out.println("Choose an option:");
-    		System.out.println("1. Show all the records.");
-    		System.out.println("2.Insert a ner recordr.");
-    		System.out.println("3. Modify a record.");
-    		System.out.println("4. Delete a record.");
-    		System.out.println("5. Delete all the records.");
-    		System.out.println("6. Exit.");
-    		option = reader.nextLine();
+    		option = showMenu(reader);
     	
     	switch (Integer.parseInt(option)){
     	
     	case 1:		
-    	sql="select * from customers";
-    	ResultSet result = statement.executeQuery(sql);
-    	while (result.next()) {
-			//System.out.print("ID: " + resultSet.getString(1));
-			//System.out.println(" Name: " + resultSet.getString(2));
-			System.out.print("ID: " + result.getInt("id"));
-			System.out.println(" Name: " + result.getString("name"));
-		}
-    	result.close();
+			showRecords(statement);
     		break;
     		
     	case 2:		
-    		
-    		System.out.println("Please enter the ID of the customer:");
-    		id = reader.nextLine();
-    		System.out.println("Please enter the name of the customer:");
-    		name = reader.nextLine();
-    		preparedStatement = connection.prepareStatement("insert into customers values (?,?)");
-    		preparedStatement.setInt(1,Integer.parseInt(id));
-    		preparedStatement.setString(2,name);
-    		preparedStatement.addBatch();
-    		preparedStatement.executeBatch();
-    		
+    		enterRecord(connection, reader);
     		break;
     		
     	case 3:	
-    		String change;
-    		System.out.println("Enter the id of a customer for do the changed:");
-    		change = reader.nextLine();
-    		System.out.println("Please enter the ID of the customer:");
-    		id = reader.nextLine();
-    		System.out.println("Please enter the name of the customer:");
-    		name = reader.nextLine();
-    		preparedStatement = connection.prepareStatement("update customers set id=(?), name=(?) where id =(?)");
-    		preparedStatement.setInt(1,Integer.parseInt(id));
-    		preparedStatement.addBatch();
-    		preparedStatement.setString(2,name);
-    		preparedStatement.addBatch();
-    		preparedStatement.setString(3,change);
-    		preparedStatement.addBatch();
-    		preparedStatement.executeBatch();
+    		changeRecord(connection, reader);
     		break;
     		
     	case 4:	
-    		String delete;
-    		System.out.println("Enter the id of a customer for delete it:");
-    		delete = reader.nextLine();
-    		preparedStatement = connection.prepareStatement("delete from customers where id=(?)");
-    		preparedStatement.setInt(1,Integer.parseInt(delete));
-    		preparedStatement.addBatch();
-    		preparedStatement.executeBatch();
+    		deleteRecord(connection, reader);
     		break;
     		
     	case 5:
-    		sql="delete from customers";
-    		statement.executeUpdate(sql);
+    		deleteAll(statement);
     		break;
     		
     	case 6:
@@ -119,4 +73,119 @@ public class App
     	}while(Integer.parseInt(option) !=6);
     	connection.close();
     }
+
+	/**
+	 * Delete all the records
+	 * @param statement
+	 * @throws SQLException
+	 */
+	private static void deleteAll(Statement statement) throws SQLException {
+		String sql;
+		sql="delete from customers";
+		statement.executeUpdate(sql);
+	}
+
+	/**
+	 * Delete one record
+	 * @param connection
+	 * @param reader
+	 * @throws SQLException
+	 */
+	private static void deleteRecord(Connection connection, Scanner reader)
+			throws SQLException {
+		PreparedStatement preparedStatement;
+		String delete;
+		System.out.println("Enter the id of a customer for delete it:");
+		delete = reader.nextLine();
+		preparedStatement = connection.prepareStatement("delete from customers where id=(?)");
+		preparedStatement.setInt(1,Integer.parseInt(delete));
+		preparedStatement.addBatch();
+		preparedStatement.executeBatch();
+	}
+
+	/**
+	 * Change one record
+	 * @param connection
+	 * @param reader
+	 * @throws SQLException
+	 */
+	private static void changeRecord(Connection connection, Scanner reader)
+			throws SQLException {
+		PreparedStatement preparedStatement;
+		String id;
+		String name;
+		String change;
+		System.out.println("Enter the id of a customer for do the changed:");
+		change = reader.nextLine();
+		System.out.println("Please enter the ID of the customer:");
+		id = reader.nextLine();
+		System.out.println("Please enter the name of the customer:");
+		name = reader.nextLine();
+		preparedStatement = connection.prepareStatement("update customers set id=(?), name=(?) where id =(?)");
+		preparedStatement.setInt(1,Integer.parseInt(id));
+		preparedStatement.addBatch();
+		preparedStatement.setString(2,name);
+		preparedStatement.addBatch();
+		preparedStatement.setString(3,change);
+		preparedStatement.addBatch();
+		preparedStatement.executeBatch();
+	}
+
+	/**
+	 * Enter one record
+	 * @param connection
+	 * @param reader
+	 * @throws SQLException
+	 */
+	private static void enterRecord(Connection connection, Scanner reader)
+			throws SQLException {
+		PreparedStatement preparedStatement;
+		String id;
+		String name;
+		System.out.println("Please enter the ID of the customer:");
+		id = reader.nextLine();
+		System.out.println("Please enter the name of the customer:");
+		name = reader.nextLine();
+		preparedStatement = connection.prepareStatement("insert into customers values (?,?)");
+		preparedStatement.setInt(1,Integer.parseInt(id));
+		preparedStatement.setString(2,name);
+		preparedStatement.addBatch();
+		preparedStatement.executeBatch();
+	}
+
+	/**
+	 * Show all records
+	 * @param statement
+	 * @throws SQLException
+	 */
+	private static void showRecords(Statement statement) throws SQLException {
+		String sql;
+		sql="select * from customers";
+    	ResultSet result = statement.executeQuery(sql);
+    	while (result.next()) {
+			//System.out.print("ID: " + resultSet.getString(1));
+			//System.out.println(" Name: " + resultSet.getString(2));
+			System.out.print("ID: " + result.getInt("id"));
+			System.out.println(" Name: " + result.getString("name"));
+		}
+    	result.close();
+	}
+
+	/**
+	 * Show a menu
+	 * @param reader
+	 * @return
+	 */
+	private static String showMenu(Scanner reader) {
+		String option;
+		System.out.println("Choose an option:");
+		System.out.println("1. Show all the records.");
+		System.out.println("2.Insert a new recordr.");
+		System.out.println("3. Modify a record.");
+		System.out.println("4. Delete a record.");
+		System.out.println("5. Delete all the records.");
+		System.out.println("6. Exit.");
+		option = reader.nextLine();
+		return option;
+	}
 }
